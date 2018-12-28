8e727b5cbb09986db0d22b6419f8e78acdaed7ac
DoomedPerson <37386791+DoomedPerson@users.noreply.github.com>
2018-12-28 11:25:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const FFMPEG = require('ffmpeg');
-const FFMPEG = require('ytdl');
+const ytdl = require('ytdl-core');
 
 
 
