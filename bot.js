53e00e801ea2a319d491e01eeb64ba0b536409dc
DoomedPerson <37386791+DoomedPerson@users.noreply.github.com>
2019-03-08 21:28:28 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
         client.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
         client.user.setPresence({
             game: {
-                name: client.user.guild.memberCount,
+                name: client.guilds.get("474688021342453780").memberCount,
                 type: 3 // 1: Playing, 2: Listening, 3: Watching
             }
         });
